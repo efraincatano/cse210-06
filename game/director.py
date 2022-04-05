e51d88804296095236db7f2bb522fd6545134450
Efrain Cataño <efrain.catano97@gmail.com>
2022-04-05 19:47:49 -0400

    printing the score

--- game/director.py
+++ game/director.py
@@ -27,10 +27,11 @@ class Director:
         duck = pyray.load_texture(self._duck.get_image())
         cougar_width = cougar.width
         cougar_height = cougar.height
-            
+        score = 0    
         while self._window.is_window_open():
 
             gotcha = False
+            
 
             while not gotcha:
 
@@ -41,24 +42,26 @@ class Director:
                 pyray.clear_background((0,0,0))
                 pyray.draw_texture_ex(cougar, random_positon, 0, 0.2, (255,255,255))
                 WHITE = (123, 255, 255)
-                pyray.draw_text("Score", 100, 100, 50, WHITE)
-                
+
+            
                 random_positon = Point(random.randint(600, 1000), random.randint(300, 600))
 
                 self._cougar.reset(random_positon)
     
                 pyray.end_drawing()
-
                 if raylib.IsMouseButtonDown(0):
                     mouse_x = pyray.get_mouse_x()
                     mouse_y = pyray.get_mouse_y()
                     print(f"Cougar x = {self._cougar.get_position().get_x()}")
-
                     if mouse_x in range(self._cougar.get_position().get_x() - 200, self._cougar.get_position().get_x() + 200) and mouse_y in range(self._cougar.get_position().get_y() - 200, self._cougar.get_position().get_y() + 200):
                         print("Gotcha!")
                         print(f"Cougar x = {self._cougar.get_position().get_x()}")
                         print(f"Cougar y = {self._cougar.get_position().get_y()}")
                         gotcha = True
+                        score +=1
+
+                pyray.draw_text(f"Score: {str(score)}", 100, 100, 50, WHITE) 
+
 
         pyray.unload_texture(cougar)
             
